7e599e21a26c6e96bf947c8da4111860e5ecd1ab
Ario Bintang <ariobintangn@gmail.com>
2023-05-11 16:25:01 +0700

    finished with feature

--- talentlytica-test/src/App.js
+++ talentlytica-test/src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import Table from './Views/Table'
+
 import ScoreTable from './Views/ScoreTable';
 import './App.css';
 
@@ -8,8 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <ScoreTable/> */}
-        <Table/>
-        <h1>TESTING YA'll</h1>
+        <ScoreTable/>
       </header>
     </div>
   );
--- talentlytica-test/src/Views/ScoreTable.js
+++ talentlytica-test/src/Views/ScoreTable.js
@@ -1,114 +1,101 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function ScoreTable() {
-  const [scores, setScores] = useState({
-    criteria_1: {
-      student_1: 0,
-      student_2: 0,
-    },
-    criteria_2: {
-      student_1: 0,
-      student_2: 0,
-    },
-  });
-  
+  const [scores, setScores] = useState({});
+  const [showResult, setShowResult] = useState(false)
 
-  const handleScoreChange = (event) => {
-    const { criteria, student, value } = event.target.dataset;
-    console.log(criteria);
-    console.log(value, "VALUE");
-    setScores((prevScores) => ({
-      ...prevScores,
-      [criteria]: {
-        ...prevScores[criteria],
-        [student]: parseInt(value),
-      },
-    })); 
-    console.log(student.value);
-    console.log("changinggg ni abis");
-   
+  const handleScoreChange = (event, indexStudent, indexCriteria) => {
+    setShowResult(false)
+    console.log(
+      indexStudent,
+      indexCriteria,
+      "indexStudent",
+      event.target.value
+    );
+    if (!scores[`aspek_penilaian_${indexCriteria + 1}`]) {
+      setScores({
+        ...scores,
+        [`aspek_penilaian_${indexCriteria + 1}`]: {},
+      });
+    }
+    setScores({
+      ...scores,
+      [`aspek_penilaian_${indexCriteria + 1}`]: {
+        ...scores[`aspek_penilaian_${indexCriteria + 1}`],
+        [`mahasiswa_${indexStudent+1}`]: parseInt(event.target.value)
+      }
+    })
+    
   };
 
+  useEffect(() => {
+    console.log(scores, "SCORE NOW");
+  }, [scores]);
+
   const handleSave = () => {
-    // fetch('/scores', {
-    //   method: 'POST',
-    //   body: JSON.stringify(scores),
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => console.log(data))
-    //   .catch((error) => console.error(error));
+    setShowResult(true)
     console.log(scores);
   };
 
-
   return (
     <div>
-      <table>
+      <table style={{tableLayout:"auto", borderColor:"white", border:12}}>
         <thead>
           <tr>
-            <th>Student</th>
-            <th>Criteria 1</th>
-            <th>Criteria 2</th>
+            <th>Nama Mahasiswa</th>
+            <th>Aspek Penilaian 1</th>
+            <th>Aspek Penilaian 2</th>
+            <th>Aspek Penilaian 3</th>
+            <th>Aspek Penilaian 4</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Student 1</td>
-            <td>
-              <select
-                data-criteria="criteria_1"
-                data-student="student_1"
-                onChange={handleScoreChange}
-              >
-                <option value="0">-</option>
-                <option value="TESTING">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
-            </td>
-            <td>
-              <select
-                data-criteria="criteria_2"
-                data-student="student_1"
-                onChange={handleScoreChange}
-              >
-                <option value="0">-</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td>Student 2</td>
-            <td>
-              <select
-                data-criteria="criteria_1"
-                data-student="student_2"
-                onChange={handleScoreChange}
-              >
-                <option value="0">-</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
-            </td>
-            <td>
-              <select
-                data-criteria="criteria_2"
-                data-student="student_2"
-                onChange={handleScoreChange}
-              >
-                <option value="0">-</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
-            </td>
-          </tr>
+          {Array(10)
+            .fill("")
+            .map((el, indexStudent) => {
+              return (
+                <tr>
+                  <td>{`Mahasiswa_${indexStudent + 1}`}</td>
+                  {Array(4)
+                    .fill(1)
+                    .map((el, indexCriteria) => {
+                      return (
+                        <td>
+                          <select
+                            onChange={(event) =>
+                              handleScoreChange(
+                                event,
+                                indexStudent,
+                                indexCriteria
+                              )
+                            }
+                          >
+                            <option value="0" selected disabled>-</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                          </select>
+                        </td>
+                      );
+                    })}
+                </tr>
+              );
+            })}
         </tbody>
       </table>
-      <button onClick={handleSave}>Save</button>
+      <button onClick={handleSave} style={{backgroundColor:"black", color:"white"}}>SIMPAN</button>
+
+    
+      {showResult && <div>
+        {JSON.stringify(scores, null, 2)}
+      </div>}
     </div>
   );
 }
